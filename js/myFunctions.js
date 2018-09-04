5bcdd22396bbdcb9f2529472e4370938d6cc3535
JamesCooper97 <james.cooper97@outlook.com>
2018-09-04 11:45:23 +0100

    Added style of navbar

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -4,6 +4,12 @@ window.onscroll = function() {myFunction()};
             if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
                 navbar.className = "Navbar" + "animate-top" + "white";
             } else {
-                navbar.className = navbar.className.replace("animate-top white", "");
+                navbar.className = navbar.className.replace("animate-top white","");
             }
+        }
+
+window.onmouseover = function() {hoverBar()};
+        function hoverBar() {
+            var hoverBar = document.getElementsByClassName("bar-item");
+            hoverBar.className
         }
